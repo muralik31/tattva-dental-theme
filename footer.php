7bb4f0951e11d2128702e7225fd0677677bee2b0
mkondapally <mkondapally@salesforce.com>
2025-12-23 21:37:06 +0530

    Revert "Fix footer policy links to use /privacy-policy slug"
    This reverts commit 27bdd59b9df35a4b15527c7c967e5581ee34c47b.

--- footer.php
+++ footer.php
@@ -121,9 +121,9 @@
         <div class="footer-bottom">
             <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
             <p>
-                <a href="<?php echo esc_url(home_url('/privacy-policy#terms')); ?>">Terms & Conditions</a> | 
-                <a href="<?php echo esc_url(home_url('/privacy-policy#privacy')); ?>">Privacy Policy</a> | 
-                <a href="<?php echo esc_url(home_url('/privacy-policy#refund')); ?>">Refund Policy</a>
+                <a href="<?php echo esc_url(home_url('/policies#terms')); ?>">Terms & Conditions</a> | 
+                <a href="<?php echo esc_url(home_url('/policies#privacy')); ?>">Privacy Policy</a> | 
+                <a href="<?php echo esc_url(home_url('/policies#refund')); ?>">Refund Policy</a>
             </p>
         </div>
     </div>
